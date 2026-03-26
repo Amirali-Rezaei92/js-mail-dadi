b953119674819adb16df8489f6ceafccc6b95a7a
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-03-27 00:14:25 +0100

    Completed full code implementation-Gioco dei dadi
    Completato il codice dell’intero programma

--- Gioco dei dadi/script.js
+++ Gioco dei dadi/script.js
@@ -7,9 +7,23 @@
 //Gioco dei dadi
 
 //1. Genero un numero casuale da 1 a 6 per il giocatore.
+let numeroGiocatore = Math.floor(Math.random() * 6) + 1;
 //2. Genero un numero casuale da 1 a 6 per il computer.
+let numeroComputer = Math.floor(Math.random() * 6) + 1;
+
 //3. Con  IF& elce Stampo entrambi i risultati per mostrarli all'utente.
+console.log(`Numero di computer è: ${numeroComputer}`);
+console.log(`Numero di Giocatore è: ${numeroGiocatore} `);
+
+
 //4. Confronto i due numeri:
-//- Se il numero del giocatore è più alto, vince il giocatore.
-//- Se il numero del computer è più alto, vince il computer.
-//- Se i numeri sono uguali, la partita finisce in pareggio.
+if(numeroGiocatore>numeroComputer){
+    console.log(`Giocatore ha vinto🙆‍♂️`); //- Se il numero del giocatore è più alto, vince il giocatore.
+}else if(numeroGiocatore<numeroComputer){//- Se il numero del computer è più alto, vince il computer.
+    console.log(`Computer ha vinto 🖥️`);  
+} else {
+    console.log(`PARI`);//- Se i numeri sono uguali, la partita finisce in pareggio.
+    
+}
+
+
