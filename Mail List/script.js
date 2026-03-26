45344f36b7bdeb4a76eb5b77040ee98191d0da97
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-03-26 20:19:05 +0100

    Added comments to all program steps Aggiunti i commenti a tutte le fasi del programma

--- Mail List/script.js
+++ Mail List/script.js
@@ -1,1 +1,12 @@
 
+//Questo programma controlla se l'email inserita dall'utente 
+//è presente nella lista delle email degli invitati. 
+//Se l'email viene trovata, l'utente può entrare; 
+//altrimenti l'accesso viene negato.
+
+//1. Creo una variabile booleana chiamata trovato e la imposto su false.
+//2. Creo un array che contiene la lista delle email degli invitati.
+//3. Chiedo all’utente di inserire la sua email tramite prompt.
+//4. Imposto un ciclo for per controllare se l’email inserita è presente nella lista.
+//5. Se l’email è presente, imposto trovato = true; altrimenti rimane false.
+//6. Scrivo una condizione if: se trovato è true, stampo un messaggio che dice all’utente che può entrare; altrimenti stampo che non può entrare.
