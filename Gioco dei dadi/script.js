e121ae058260a1a7b064775ad5738a4eaedbe49c
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-03-27 00:08:45 +0100

    Added comments to all program steps
     Aggiunti i commenti a tutte le fasi del programma - Gioco dei dadi

--- Gioco dei dadi/script.js
+++ Gioco dei dadi/script.js
@@ -1,5 +1,15 @@
+// Gioco dei dadi:
+//Il programma genera due numeri casuali da 1 a 6, uno per il giocatore
+// e uno per il computer. Successivamente confronta i due risultati
+//per determinare chi ha ottenuto il punteggio più alto e quindi il vincitore.
 
 
-let numeroGiocatore =  Math.floor(Math.random()*6)+1;
-let numeroComputer = Math.floor(Math.random()*6)+1;
+//Gioco dei dadi
 
+//1. Genero un numero casuale da 1 a 6 per il giocatore.
+//2. Genero un numero casuale da 1 a 6 per il computer.
+//3. Con  IF& elce Stampo entrambi i risultati per mostrarli all'utente.
+//4. Confronto i due numeri:
+//- Se il numero del giocatore è più alto, vince il giocatore.
+//- Se il numero del computer è più alto, vince il computer.
+//- Se i numeri sono uguali, la partita finisce in pareggio.
