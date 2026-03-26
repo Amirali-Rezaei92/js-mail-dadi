2dcc108d192506b710ff5491202d981b607f6056
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-03-26 20:42:37 +0100

    Completed full code implementation
    Completato il codice dell’intero programma

--- Mail List/script.js
+++ Mail List/script.js
@@ -5,8 +5,35 @@
 //altrimenti l'accesso viene negato.
 
 //1. Creo una variabile booleana chiamata trovato e la imposto su false.
+let trovato = false;
 //2. Creo un array che contiene la lista delle email degli invitati.
+let listaDelleMail = ['examplemail1@gmail.com',
+    'examplemail2@gmail.com',
+    'examplemail3@yahoo.com',
+    'examplemail4@hotmail.com',
+    'bluefox128@gmail.com',
+    'pixelstorm.dev@outlook.com',
+    'neonwave77@yahoo.com',
+    'silentcoder.pro@protonmail.com',
+    'lunar.byte88@gmail.com',
+    'orbitdesign.lab@icloud.com',
+    'shadowgrid01@outlook.com',
+    'quantum.flow23@yahoo.com',
+    'nova.spark.dev@gmail.com',
+    'cyberleaf.mail@protonmail.com'];
 //3. Chiedo all’utente di inserire la sua email tramite prompt.
+let mail = prompt("Inserisci l'indirizzo email");
+
 //4. Imposto un ciclo for per controllare se l’email inserita è presente nella lista.
-//5. Se l’email è presente, imposto trovato = true; altrimenti rimane false.
-//6. Scrivo una condizione if: se trovato è true, stampo un messaggio che dice all’utente che può entrare; altrimenti stampo che non può entrare.
+for (i = 0; i < listaDelleMail.length; i++) {
+    if (mail === listaDelleMail[i]) {
+        trovato = true;
+    }
+}
+//5. Se l’email è presente, imposto trovato = true; altrimenti rimane false e Scrivo una condizione if: se trovato è true, 
+// stampo un messaggio che dice all’utente che può entrare; altrimenti stampo che non può entrare.
+if (trovato === true) {
+    console.log(`✅Utente con Mail ${mail} Sei tra gli invitati✅`);
+} else {
+    console.log(`❌Utente con Mail ${mail} purtroppo non sei invitato❌`);
+}
